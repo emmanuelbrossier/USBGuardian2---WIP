0605ff2f02b9d3f9d5101931847f780088a222ad
manu <ebrossier@hotmail.fr>
2022-08-11 15:52:43 +0200

    Update progress.sh

--- opt/USBGuardian/scripts/progress.sh
+++ opt/USBGuardian/scripts/progress.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 #Aller dans le dossier de la clé USB
@@ -21,3 +22,37 @@ do
         #Soustraction du nombre de fichiers supprimés au nombre total de fichiers
         current=$((current - filesRemoved))
         totalR=$((total - filesRemoved))
+        if [ "$totalRealTime" -lt "$totalR" ]
+        then
+        echo "USB Removed"
+        #Arrêt du process Clamscan
+        sudo killall clamscan
+        exit
+fi
+
+#Les 25 premier pourcent sont dédiés au chargement des librairies clamav
+
+if [ "$percentage" -lt 25]
+then
+        percentage=$((percentage +1))
+fi
+then
+        #Conversion du nombre de fichiers scannés en porcentage sur un total de 75 pourcent
+        current=$((current * 75))
+        percentage=$((current / totalRealTime))
+        #On ajoute les 25 pourcent précédemment chargé pour les librairies clamav
+        percentage=$((percentage + 25))
+        sDuration=0.125
+fi
+
+echo "$percentage %"
+
+if [ "$percentage" -gt 99 ]
+then
+        exit
+fi
+
+#Ralentir la vitesse de l'écoute
+sleep "$sDuration"
+done
+
